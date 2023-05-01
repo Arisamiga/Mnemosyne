dba73602fe7f3629620b863230a977524565678d
Aris <64918822+Arisamiga@users.noreply.github.com>
2023-05-01 16:06:02 +0100

    Added basic functionality with Choices

--- src/main.c
+++ src/main.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
 
+// Declare functions after main
+int initialChoice(int);
+int scan(void);
+int cancel(void);
+int credits(void);
+
 int main(void)
 {
-    printf("Works!\n");
+    printf("\nWelcome to Mnemosyne!\n\n To get started:\n  Type '1' to start the scan,\n  Type '2' to cancel,\n  Type '3' if you want to see credits.\n");
+    int choices = 0; 
+    scanf("%d", &choices);
+    initialChoice(choices);
+    return 0;
+}
+
+int initialChoice(int choices) {
+    if (choices == 1)
+    {
+        scan();
+    }
+    else if (choices == 2)
+    {
+        cancel();
+    }
+    else if (choices == 3)
+    {
+        credits();
+    }
+    else
+    {
+        printf("Invalid input, please try again.\n");
+        int choice = 0;
+        scanf("%d", &choice);
+        initialChoice(choice);
+    }
+}
+
+int scan(void)
+{
+    printf("Scanning...\n");
+    return 0;
+}
+
+int cancel(void)
+{
+    printf("Cancelling... Thanks so much for using Mnemosyne!\n");
+    return 0;
+}
+
+int credits(void)
+{
+    printf("Credits: (SoonTM)\n");
     return 0;
 }
