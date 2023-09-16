3d273712e879197c9e85e3858f40da4baa9e4dbc
Aris <arisamiga@gmail.com>
2023-09-16 13:32:49 +0100

    Using snprintf

--- src/window.c
+++ src/window.c
@@ -510,7 +510,7 @@ void processEvents(Object *windowObject,
 
 								if (parentPath[strlen(parentPath) - 1] != ':')
 								{
-									strcat(parentPath, "/");
+									snprintf(parentPath, MAX_BUFFER, "%s/", parentPath);
 								}
 
 								updateBottomTextW2Text(bottomText, windowObject, "Scanning: ", parentName, FALSE);
